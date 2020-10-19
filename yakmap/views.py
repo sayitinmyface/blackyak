6932fd11a841b53e603ec7908accaacef29bc71e
sayitinmyface <leo99roo@gmail.com>
2020-10-19 09:28:25 +0900

    10190928/sayit

--- yakmap/views.py
+++ yakmap/views.py
@@ -1,23 +1,17 @@
 from django.shortcuts import render
 import folium
 from pymongo import MongoClient
-import pyowm
 # Create your views here.
 # 지도에 산 모든 위치 표시 
 def home(req):
     # 처음 전체 화면 lat,lon
     lat_lon = [36.0040,128.1540]
     m = folium.Map(location=lat_lon,zoom_start=7,tiles='Stamen Terrain')
-    # 날씨 정보
-    owm = pyowm.OWM('5b457f895ab57ef2daac2b9e32db5319')#api key value
-    mgr = owm.weather_manager()
     #산 정보 get
     list_info = getInfo('mountain_info') 
     # 
     for f_info in list_info:
         lat_lon = [float(f_info['lat']),float(f_info['lon'])]
-        # obj = mgr.weather_at_coords(float(f_info['lat']),float(f_info['lon']))
-        # w = obj.weather
         html = f'''
                     <table border="1">
                         <tr>
@@ -40,13 +34,16 @@ def home(req):
     m = m._repr_html_
     return render(req,'yakmap/home.html',{'map':m})
 
-#DB find 
-def getInfo(collection_name):
-    db_url = db_url = 'mongodb://192.168.219.105:27017'
+#DB 산 정보 
+def getInfo(db_url = 'mongodb://192.168.0.179:27017',collection_name):
     with MongoClient(db_url) as client:
         result = list(client['mydb'][collection_name].find())
     return result
-# 
+# DB 날씨
+def getWeatherinfo(db_url = 'mongodb://192.168.0.179:27017',lat,lon):
+    with MongoClient(db_url) as client:
+        result = list(client['mydb'][collection_name].find({'lat':lat,'lon':lon}))
+    return result
 
     
     
